74bf8afdf129b2002a2aefca26c30e4dde21f087
Martina Köhli <martina.koehli@bve.be.ch>
2018-02-20 12:52:01 +0100

    #414: Legendentitel werden nun z.T. auto erstellt.

--- DDSync/Legend.py
+++ DDSync/Legend.py
@@ -28,11 +28,19 @@ class Legend(object):
         
         # Legende deutsch
         self.leg_bezeichnung_mittel_de_de = unicode(xpatheval("string(bee:legendTitleDE/gco:CharacterString)"))
-        self.leg_bezeichnung_mittel_fr_de = unicode(xpatheval("string(bee:legendTitleFR/gco:CharacterString)"))
-        
+
         # Legende französisch
-        self.leg_bezeichnung_mittel_de_fr = unicode(xpatheval("string(bee:legendTitleDE/gmd:PT_FreeText/gmd:textGroup/gmd:LocalisedCharacterString[@locale = '#FR'])"))
         self.leg_bezeichnung_mittel_fr_fr = unicode(xpatheval("string(bee:legendTitleFR/gmd:PT_FreeText/gmd:textGroup/gmd:LocalisedCharacterString[@locale = '#FR'])"))
+
+        # Sprachkürzel ergänzen
+        if not self.leg_bezeichnung_mittel_de_de.endswith(' (de)'):
+            self.leg_bezeichnung_mittel_de_de += ' (de)'
+        if not self.leg_bezeichnung_mittel_fr_fr.endswith(' (fr)'):
+            self.leg_bezeichnung_mittel_fr_fr += ' (fr)'
+            
+        # Legendenfälle ergänzen
+        self.leg_bezeichnung_mittel_fr_de = self.leg_bezeichnung_mittel_fr_fr.replace(' (fr)',' (de)')
+        self.leg_bezeichnung_mittel_fr_fr = self.leg_bezeichnung_mittel_de_de.replace(' (de)',' (fr)')
         
         self.sql_statements.append("INSERT INTO %s.tb_legende (LEG_OBJECTID, EZS_OBJECTID, SPR_OBJECTID, LEG_BEZEICHNUNG, LEG_BEZEICHNUNG_MITTEL_DE, lEG_BEZEICHNUNG_MITTEL_FR) VALUES (%s, %s, 1, '%s', '%s', '%s')" % (dd_schema, self.leg_objectid_de, self.ezs_objectid, self.leg_bezeichnung, clean(self.leg_bezeichnung_mittel_de_de), clean(self.leg_bezeichnung_mittel_fr_de))) 
         self.sql_statements.append("INSERT INTO %s.tb_legende (LEG_OBJECTID, EZS_OBJECTID, SPR_OBJECTID, LEG_BEZEICHNUNG, LEG_BEZEICHNUNG_MITTEL_DE, lEG_BEZEICHNUNG_MITTEL_FR) VALUES (%s, %s, 2, '%s', '%s', '%s')" % (dd_schema, self.leg_objectid_fr, self.ezs_objectid, self.leg_bezeichnung, clean(self.leg_bezeichnung_mittel_de_fr), clean(self.leg_bezeichnung_mittel_fr_fr)))
