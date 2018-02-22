13cb6daf81a66d584b392bada632886695062c84
Martina Köhli <martina.koehli@bve.be.ch>
2018-02-22 11:17:49 +0100

    #414: Korrekte XML-Legende auslesen

--- DDSync/Legend.py
+++ DDSync/Legend.py
@@ -30,7 +30,7 @@ class Legend(object):
         self.leg_bezeichnung_mittel_de_de = unicode(xpatheval("string(bee:legendTitleDE/gco:CharacterString)"))
 
         # Legende französisch
-        self.leg_bezeichnung_mittel_fr_fr = unicode(xpatheval("string(bee:legendTitleFR/gmd:PT_FreeText/gmd:textGroup/gmd:LocalisedCharacterString[@locale = '#FR'])"))
+        self.leg_bezeichnung_mittel_fr_fr = unicode(xpatheval("string(bee:legendTitleDE/gmd:PT_FreeText/gmd:textGroup/gmd:LocalisedCharacterString[@locale = '#FR'])"))
 
         # Sprachkürzel ergänzen
         if not self.leg_bezeichnung_mittel_de_de.endswith(' (de)'):
@@ -40,7 +40,7 @@ class Legend(object):
             
         # Legendenfälle ergänzen
         self.leg_bezeichnung_mittel_fr_de = self.leg_bezeichnung_mittel_fr_fr.replace(' (fr)',' (de)')
-        self.leg_bezeichnung_mittel_fr_fr = self.leg_bezeichnung_mittel_de_de.replace(' (de)',' (fr)')
+        self.leg_bezeichnung_mittel_de_fr = self.leg_bezeichnung_mittel_de_de.replace(' (de)',' (fr)')
         
         self.sql_statements.append("INSERT INTO %s.tb_legende (LEG_OBJECTID, EZS_OBJECTID, SPR_OBJECTID, LEG_BEZEICHNUNG, LEG_BEZEICHNUNG_MITTEL_DE, lEG_BEZEICHNUNG_MITTEL_FR) VALUES (%s, %s, 1, '%s', '%s', '%s')" % (dd_schema, self.leg_objectid_de, self.ezs_objectid, self.leg_bezeichnung, clean(self.leg_bezeichnung_mittel_de_de), clean(self.leg_bezeichnung_mittel_fr_de))) 
         self.sql_statements.append("INSERT INTO %s.tb_legende (LEG_OBJECTID, EZS_OBJECTID, SPR_OBJECTID, LEG_BEZEICHNUNG, LEG_BEZEICHNUNG_MITTEL_DE, lEG_BEZEICHNUNG_MITTEL_FR) VALUES (%s, %s, 2, '%s', '%s', '%s')" % (dd_schema, self.leg_objectid_fr, self.ezs_objectid, self.leg_bezeichnung, clean(self.leg_bezeichnung_mittel_de_fr), clean(self.leg_bezeichnung_mittel_fr_fr)))
