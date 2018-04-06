bf781916a67b1a2c59f706555078db2da116d821
Martina Köhli <martina.koehli@bve.be.ch>
2018-04-06 10:24:43 +0200

    Revert "Checskript wird nur noch über CMD aufgerufen, ohne Toolbox"
    This reverts commit d9569b3c6c56fd962826facf5b20740944f86cbd.

--- DDSync/helpers/check_helper.py
+++ DDSync/helpers/check_helper.py
@@ -1,17 +1,19 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, division, print_function, unicode_literals
-import os
+import arcpy
 
 def run_checkscript_normierung(config, code, jahr, version):
 
     toolbox = config['CHECKSCRIPT_NORMIERUNG']['toolbox']
     
-    result = os.popen(toolbox + " " + code + " " + str(jahr) + " " + str(version) + " true true true true false").read()
-    result = [i for i in result.decode('cp1252').split('\n') if i][-1]
+    arcpy.ImportToolbox(toolbox)
+    result = arcpy.CheckGeoproduct_tatata(Geoprodukt=code,Jahr=jahr,Version=version,Pruefe_GeoDBProzess="true",Pruefe_Filesystem="true",Pruefe_GeoDBmeta="true",Pruefe_Geodaten="true",Pruefe_GeoDBmeta_DD="false")
 
-    if unicode(result) == "Successfull":
+    if unicode(result[0]) == "true":
         status_checkscript_normierung = True
     else:
-        status_checkscript_normierung = False  
+        status_checkscript_normierung = False
+        
+    arcpy.RemoveToolbox(toolbox)
                         
     return status_checkscript_normierung
