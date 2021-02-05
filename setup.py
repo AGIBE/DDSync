0145488453a1a6605e86f7be794e531d18afdaaf
Martina Köhli <martina.koehli@bve.be.ch>
2021-02-05 16:29:21 +0100

    Anpassungen für VDI.

--- setup.py
+++ setup.py
@@ -1,16 +1,17 @@
 # -*- coding: utf-8 -*-
 # übernommen aus: https://pythonhosted.org/setuptools/setuptools.html#id24
 import ez_setup
-from DDSync import __version__
+
 
 ez_setup.use_setuptools()
 
 from setuptools import setup, find_packages
 setup(
       name = "DDSync",
-      packages = find_packages(),
-      version = __version__,
+      packages = find_packages(where="src"),
+      version = '1.5.0',
       package_data={'': ["*.fmw"]},
+      package_dir = {"": "src"},
       # Abhängigkeiten
       install_requires = ["configobj==5.0.6", "cx-Oracle>=5.1.3", "requests>=2.20.0", "PyMySQL==0.7.4", "lxml==3.4.4", "python-keyczar==0.715", "psycopg2>=2.7.3.1"],
       # PyPI metadata
@@ -20,7 +21,7 @@ setup(
       url = "https://www.be.ch/geoportal",
       entry_points={
            'console_scripts': [
-                'DDSync = DDSync.helpers.commandline_helper:main'
+                'DDSync = DDSync.__main__:main'
             ]         
       }
       # https://pythonhosted.org/setuptools/setuptools.html#automatic-script-creation
